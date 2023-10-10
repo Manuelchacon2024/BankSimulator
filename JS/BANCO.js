8f287c4f9c578519632e5f8da66cb40288b452b7
Manuelchacon2024 <santos.garcia2024@hilasal.superate.org.sv>
2023-10-10 11:21:09 -0600

    Trabajo en proceso

---
+++ JS/BANCO.js
@@ -0,0 +1,84 @@
+saldoPrincipal = 2500.00;
+conteo = 3
+
+function iniciarsesion(){
+    var user, pass;
+
+    user = document.getElementById("usuario").value;
+    pass = document.getElementById("contraseña").value;
+
+    if(pass == "1234"){
+        alert("Bienvenido " + user + ", has ingresado exitosamente");
+        window.location = "opciones.html"
+    }else{
+        conteo = conteo - 1;
+            alert("Tu contraseña es incorrecta, te quedan " + conteo + " intentos.");
+        }
+    if(conteo == 0){
+        alert("Tu usuario ha sido bloqueado");
+        window.location = "banco.html"
+    }
+}
+
+function ConsultaSaldo(){ 
+    alert("Su saldo actual es de: $"+ saldoPrincipal);
+}
+
+function RetirarMonto(){
+    retiro = parseInt(prompt("Digite la cantidad a retirar:", "00.00"));
+    if(retiro > saldoPrincipal || retiro < 0){
+        alert("¡Su saldo es insuficiente para realizar esta transaccion!");
+    }else{
+
+        saldoPrincipal = saldoPrincipal - retiro;
+
+        billete20 = parseInt(retiro / 20);
+        retiro = retiro - (billete20 * 20);
+
+        billete10 = parseInt(retiro / 10);
+        retiro = retiro - (billete10 * 10);
+
+        billete5 = parseInt(retiro / 5);
+        retiro = retiro - (billete5 * 5);
+
+        billete1 = parseInt(retiro / 1);
+        retiro = retiro - (billete5 * 1);
+
+
+        entrega = "";
+
+        if(billete20 > 0){
+            entrega = entrega + "Billetes de $20 -> " + billete20 + "\n";
+        }
+        if(billete10 > 0){
+            entrega = entrega + "Billetes de $10 -> " + billete10 + "\n";
+        }
+        if(billete5 > 0){
+            entrega = entrega + "Billetes de $5 -> " + billete5 + "\n";
+        }
+        if(billete1> 0){
+            entrega = entrega + "Billetes de $1 -> " + billete1 + "\n";
+        }
+
+        alert(entrega + "\nSu saldo principal es: $"+ saldoPrincipal);
+    }
+}
+
+function AbonarCuenta(){
+    abono = parseInt(prompt("Digite la cantidad a abonar:", "0.00"));
+    if(abono > 0){
+        saldoPrincipal = saldoPrincipal + abono;
+        alert("Su nuevo saldo es: $"+ saldoPrincipal);
+    }else{
+        alert("¡Debe ingresar correctamente los datos!");
+    }
+}
+
+function aboutUs(){
+    alert("El Banco ¡Supérate! Es un proyecto realizado por los alumnos de primer año del centro ¡Supérate! Hilasal, que pretende tener el funcionamiento de un cajero automático en línea; en el cual, hemos podido poner a prueba nuestros conocimientos y también aplicar todos los conocimientos adquiridos en el mismo. \nEste banco fue creado por los estudiantes: \nMichael Ramírez, Daniela Rodríguez, Jairo Guzmán, Daniela Martínez y Geisel Ortis. \nSin duda, esta ha sido una experiencia muy desafiante y al mismo tiempo hemos adquirido muchos conocimientos.")
+}
+
+function leave(){
+    alert("¡Gracias por visitarnos!");
+    window.location = "banco.html"
+}
